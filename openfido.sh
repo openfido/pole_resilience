525b03c981d4b90fd381e2f204f2668ac2ef5aef
David P. Chassin <dchassin@slac.stanford.edu>
2022-04-25 11:15:42 -0700

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -21,10 +21,12 @@ CONFIGGLM=$(grep '^CONFIGGLM,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
 POLEGLM=$(grep '^POLEGLM,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
 RECORDERGLM=$(grep '^RECORDERGLM,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
 WEATHERGLM=$(grep '^WEATHERGLM,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
+WEATHERCSV=$(grep '^WEATHERCSV,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
+SETTINGSCSV=$(grep '^SETTINGSCSV,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
 TIMESTEP=$(grep '^TIMESTEP,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
 OPTIONS=$(grep '^OPTIONS,' ${OPENFIDO_INPUT}/config.csv | cut -f2 -d,)
 
 cd ${OPENFIDO_OUTPUT}
-cp "${OPENFIDO_INPUT}/${CONFIGGLM}" ${CONFIGGLM}
+cp "${OPENFIDO_INPUT}/${CONFIGGLM}" "${OPENFIDO_INPUT}/${WEATHERCSV}" "${OPENFIDO_INPUT}/${SETTINGSCSV}" .
 /usr/local/bin/gridlabd "${OPENFIDO_INPUT}/${WEATHERGLM}" "${OPENFIDO_INPUT}/${POLEGLM}" "${OPENFIDO_INPUT}/${NETWORKGLM}" "${OPENFIDO_INPUT}/${RECORDERGLM}" -D minimum_timestep=${TIMESTEP} ${OPTIONS}
-rm ${CONFIGGLM}
+rm -f "${CONFIGGLM}" "${WEATHERCSV}" "${SETTINGSCSV}"
